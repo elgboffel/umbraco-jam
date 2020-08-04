83a5c1d0f53a98958369433312442164168519ef
Johnny Kristensen <johnny.kristensen@akqa.com>
2020-08-04 13:36:29 +0200

    Added password

--- .circleci/publish-project.ps1
+++ .circleci/publish-project.ps1
@@ -1,20 +1,17 @@
 ﻿$solutionPath = "backend/UmbracoJAM.sln"
 $projectPath = "backend/src/Project/UmbracoJAM/code/UmbracoJAM.Project.Web.csproj"
-$clean = $true
+$clean = $false
 $nuGet = $true
-
- if ($nuGet) {
-     Write-Host "Restoring NuGet packages" -foregroundcolor green
-     nuget restore "$($solutionPath)"
- }
 
  if ($clean) {
      Write-Host "Cleaning $($solutionPath)" -foregroundcolor green
      & msbuild "$($solutionPath)" /t:Clean /m
  }
 
-Write-Host "Building $($solutionPath)" -foregroundcolor green
-& "$($msBuildExe)" "$($solutionPath)" /t:Build /m
+ if ($nuGet) {
+     Write-Host "Restoring NuGet packages" -foregroundcolor green
+     nuget restore "$($solutionPath)"
+ }
 
-Write-Host "Publishing $($solutionPath)" -foregroundcolor green
-& msbuild "$($projectPath)" /p:VisualStudioVersion=16.0 /p:DeployOnBuild=true /p:Configuration=Debug /p:PublishProfile=Properties/PublishProfiles/Production.pubxml
+Write-Host "Publishing $($solutionPath) with password: $($env:PRODUCTION_PASS)" -foregroundcolor green
+& msbuild "$($projectPath)" /p:VisualStudioVersion=16.0 /p:DeployOnBuild=true /p:Configuration=Debug /p:PublishProfile=Properties/PublishProfiles/Production.pubxml /p:WY34qkPc
